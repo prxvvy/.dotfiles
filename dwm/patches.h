0137e6f2cfe496d1ed7f86857401c2b06fefa212
prxvvy <qsk55464@gmail.com>
2022-04-15 01:24:35 -0400

    update

--- dwm/patches.h
+++ dwm/patches.h
@@ -266,7 +266,7 @@
 /* This patch adds a border around the status bar(s) just like the border of client windows.
  * https://codemadness.org/paste/dwm-border-bar.patch
  */
-#define BAR_BORDER_PATCH 1
+#define BAR_BORDER_PATCH 0
 
 /* This patch centers the WM_NAME of the currently selected window on the status bar.
  * This is compatible with the wintitle, bartabgroups, flexwintitle and awesomebar bar

138fa10bacee77175a50b71fe4a8b1370b517ef6
prxvvy <qsk55464@gmail.com>
2023-03-27 00:19:47 -0300

    update

--- nvim/lua/core/config.lua
+++ nvim/lua/core/config.lua
@@ -21,7 +21,7 @@ M.ui = {
         style = "borderless", -- borderless / bordered
     },
     statusline = {
-        theme = "vscode", -- default/vscode/vscode_colored/minimal
+        theme = "default", -- default/vscode/vscode_colored/minimal
         -- default/round/block/arrow separators work only for default statusline theme
         -- round and block will work for minimal theme only
         separator_style = "default",
